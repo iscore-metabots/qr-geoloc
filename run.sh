ad6a5f29731e0606210aecdf07ff877b1cc1d408
s-mick <smick@ensc.fr>
2016-01-15 14:26:41 +0100

    New parameters order

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@
 cam=1
 
 # Calibrate the camera based on the image previously captured
-cd ../chess-calib
-./chess-calib.xc $cam ../tmp/chess-data.yml ../tmp/calib-data.yml
+cd chess-calib
+./chess-calib.xc ../tmp/chess-data.yml ../tmp/scn-data.yml $cam ../tmp/calib-data.yml
 
 # Access the camera's video stream and process it based on the calibration and scene data
 cd ../qr-track
